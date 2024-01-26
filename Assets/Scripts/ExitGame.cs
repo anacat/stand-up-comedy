5c03cbf64642dfe29bcc9eeaf9ac1386fd8a37b5
ana <catarina.lopes.04@hotmail.com>
2024-01-26 22:49:01 +0000

    kinect stuff

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public bool exit;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (exit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
